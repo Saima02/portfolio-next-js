1ec0940ad63279f3589b63c63f8a5297a6e35717
Saima02 <96485714+Saima02@users.noreply.github.com>
2025-09-23 22:15:11 +0530

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,14 +2,14 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Logo, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "Ansari",
+  lastName: "Saima",
+  name: `Ansari Saima`,
+  role: "Web Developer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
   location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
@@ -65,7 +65,7 @@ const home: Home = {
   },
   subline: (
     <>
-      I'm Selene, a design engineer at{" "}
+      I'm Saima, a web developer at{" "}
       <Logo
         dark
         icon="/trademarks/wordmark-dark.svg"
